a2e98a7631971a52d9e6db248a3ff325879f9a77
Dave <bbec0689@gmail.com>
2021-01-19 10:37:48 -0700

    optimized code

--- script.js
+++ script.js
@@ -1,59 +1,59 @@
-const characterAmountRange = document.getElementById('characterAmountRange')
-const characterAmountNumber = document.getElementById('characterAmountNumber')
-const includeUppercaseElement = document.getElementById('includeUppercase')
-const includeNumbersElement = document.getElementById('includeNumbers')
-const includeSymbolsElement = document.getElementById('includeSymbols')
-const form = document.getElementById('passwordGeneratorForm')
-const passwordDisplay = document.getElementById('passwordDisplay')
+var characterAmountRange = document.getElementById('characterAmountRange')
+var characterAmountNumber = document.getElementById('characterAmountNumber')
+var includeUppercaseElement = document.getElementById('includeUppercase')
+var includeNumbersElement = document.getElementById('includeNumbers')
+var includeSymbolsElement = document.getElementById('includeSymbols')
+var form = document.getElementById('passwordGeneratorForm')
+var displayPass = document.getElementById('passwordDisplay')
 
-const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
-const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
-const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
-const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
-  arrayFromLowToHigh(58, 64)
+var upperCase = LowToHigh(65, 90)
+var lowerCase = LowToHigh(97, 122)
+var numCodes= LowToHigh(48, 57)
+var symCodes = LowToHigh(33, 47).concat(
+  LowToHigh(58, 64)
 ).concat(
-  arrayFromLowToHigh(91, 96)
+  LowToHigh(91, 96)
 ).concat(
-  arrayFromLowToHigh(123, 126)
+  LowToHigh(123, 126)
 )
 
-characterAmountNumber.addEventListener('input', syncCharacterAmount)
-characterAmountRange.addEventListener('input', syncCharacterAmount)
+characterAmountNumber.addEventListener('input', syncCharacter)
+characterAmountRange.addEventListener('input', syncCharacter)
 
 form.addEventListener('submit', e => {
   e.preventDefault()
-  const characterAmount = characterAmountNumber.value
-  const includeUppercase = includeUppercaseElement.checked
-  const includeNumbers = includeNumbersElement.checked
-  const includeSymbols = includeSymbolsElement.checked
-  const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
-  passwordDisplay.innerText = password
+  var characterAmount = characterAmountNumber.value
+  var includeUppercase = includeUppercaseElement.checked
+  var includeNumbers = includeNumbersElement.checked
+  var includeSymbols = includeSymbolsElement.checked
+  var password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
+  displayPass.innerText = password
 })
 
 function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
-  let charCodes = LOWERCASE_CHAR_CODES
-  if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
-  if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
-  if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+  let charCodes = lowerCase
+  if (includeUppercase) charCodes = charCodes.concat(upperCase)
+  if (includeSymbols) charCodes = charCodes.concat(symCodes)
+  if (includeNumbers) charCodes = charCodes.concat(numCodes)
   
-  const passwordCharacters = []
+  var passwordCharacters = []
   for (let i = 0; i < characterAmount; i++) {
-    const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+    var characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
     passwordCharacters.push(String.fromCharCode(characterCode))
   }
   return passwordCharacters.join('')
 }
 
-function arrayFromLowToHigh(low, high) {
-  const array = []
+function LowToHigh(low, high) {
+  var array = []
   for (let i = low; i <= high; i++) {
     array.push(i)
   }
   return array
 }
 
-function syncCharacterAmount(e) {
-  const value = e.target.value
+function syncCharacter(e) {
+  var value = e.target.value
   characterAmountNumber.value = value
   characterAmountRange.value = value
 }
